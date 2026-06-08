0eac33ab37860ebbf0f231d02653f087149a3016
Slava <anisimov.vyacheslav.21@inbox.ru>
2026-06-09 00:35:00 +0300

    db config

--- src/app/data_base/database.py
+++ src/app/data_base/database.py
@@ -1,12 +1,21 @@
-from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 from secret_data import USERNAME, PASSWORD, DATABASE
 
+URL = f"postgresql+asyncpg://{USERNAME}:{PASSWORD}@5432/{DATABASE}"
+
+async_engine = create_async_engine(
+    url=URL,
+    pool_size=5,
+    max_overflow=30,
+    pool_timeout=30,
+    echo=False
+)
+
+Async_session = async_sessionmaker(
+    bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+
 class Base(DeclarativeBase):
     ...
 
-
-async_engine = f"postgresql+asyncpg://{USERNAME}:{PASSWORD}@5432/{DATABASE}"
-
-Async_session = async_sessionmaker(bind=async_engine)
